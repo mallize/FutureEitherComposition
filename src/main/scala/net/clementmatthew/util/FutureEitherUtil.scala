d300388bc9c362f6d9dac1410d51333483d2b7e9
Matt Clement <mallize@gmail.com>
2016-03-25 16:22:46 -0500

    FutureEitherUtil

--- src/main/scala/net/clementmatthew/util/FutureEitherUtil.scala
+++ src/main/scala/net/clementmatthew/util/FutureEitherUtil.scala
@@ -3,14 +3,14 @@ package net.clementmatthew.util
 import scala.concurrent.{ExecutionContext, Future}
 
 object FutureEitherUtil {
-   def applyEither[L, R1, R2](f: (R1) => Future[Either[L, R1]], e: Either[L, R1])(implicit ec: ExecutionContext): Future[Either[L, R1]] = {
+   def applyEither[L, R1, R2](f: (R1) => Future[Either[L, R2]], e: Either[L, R1])(implicit ec: ExecutionContext): Future[Either[L, R2]] = {
       e match {
          case Left(l) => Future.successful(Left(l))
          case Right(r) => f(r)
       }
    }
 
-   def applyEither1[L, R1, R2, A](f: (R1, A) => Future[Either[L, R1]], e: Either[L, R1], s: A)(implicit ec: ExecutionContext): Future[Either[L, R1]] = {
+   def applyEither1[L, R1, R2, A](f: (R1, A) => Future[Either[L, R2]], e: Either[L, R1], s: A)(implicit ec: ExecutionContext): Future[Either[L, R2]] = {
       e match {
          case Left(l) => Future.successful(Left(l))
          case Right(r) => f(r, s)
